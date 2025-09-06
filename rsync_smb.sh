b4a58c476d025729a9e6f0f138caa6a741f76831
Denys Brekhov <denys.brekhov@gmail.com>
2025-09-06 15:08:20 +0300

    Update rsync_smb.sh

--- rsync_smb.sh
+++ rsync_smb.sh
@@ -43,7 +43,7 @@ backup_share() {
         # Видалення виключень ключем --delete-excluded                      #
         # Відпрацювання скрипта без внесення змін --dry-run                 #
         #####################################################################
-        sudo rsync -a --info=progress2 --chown=nobody:nogroup --chmod=777 --delete-during "$MOUNT_POINT/" "$target"
+        sudo rsync -a --info=progress2 --chown=nobody:nogroup --chmod=777 --delete-during "$MOUNT_POINT/" "$target"  # Власника, групу та права задати відповідно до політики безпеки
 
         # Відмонтування SMB-шари
         sudo umount "$MOUNT_POINT"
